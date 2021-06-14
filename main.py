4c3bce5e7f782afed001bfb88b7a3602d897c060
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-06-14 16:58:39 +0900

    exit

--- main.py
+++ main.py
@@ -13,24 +13,30 @@ screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
 writer = turtle.Turtle()
 writer.penup()
 writer.hideturtle()
 writer.color('black')
+
 df = pandas.read_csv("50_states.csv")
 
-correct_guess = []
-game_is_on = True
-while game_is_on:
-    guess = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
+guessed_states = []
+while len(guessed_states) < 50:
+    guess = screen.textinput(title=f"{len(guessed_states)} out of 50 guessed", prompt="What's another state's name?").title()
+    if guess == "Exit":
+        break
     if guess in df['state'].unique():
-        correct_guess.append(guess)
-        idx = df[df["state"] == guess].index.values.astype(int)[0]
-        row = df[df['state'] == guess]
-        x = row.loc[idx]['x']
-        y = row.loc[idx]['y']
-        writer.goto((x, y))
-        writer.write(guess)
+        guessed_states.append(guess)
+        # idx = df[df["state"] == guess].index.values.astype(int)[0]
+        # row = df[df['state'] == guess]
+        # x = row.loc[idx]['x']
+        # y = row.loc[idx]['y']
+        # writer.goto((x, y))
+        # writer.write(guess)
+        state_data = df[df.state == guess]
+        writer.goto(state_data.x.item(), state_data.y.item())
+        writer.write(state_data.state.item())
 
 
-turtle.mainloop()
+# turtle.mainloop()
